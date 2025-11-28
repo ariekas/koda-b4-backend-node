52b3139b68a15d3f2d3918544074af38585eeb1e
ariekas <ariariekas@gmail.com>
2025-11-28 21:58:04 +0700

    feat: upadate code edit discount and create calculation to product_discount

--- src/repositorys/discount.js
+++ src/repositorys/discount.js
@@ -7,52 +7,66 @@ export async function create(data) {
 
     if (!product) throw new Error("Product tidak ditemukan");
 
-    const priceDiscount = product.price - (product.price * data.discount / 100)
+  const priceDiscount = product.price - (product.price * data.discount) / 100;
 
-     await prisma.discount.create({
-        data: {
-            ...data,
-            productId: data.productId ? Number(data.productId) : 0
-        }
-    })
+  await prisma.discount.create({
+    data: {
+      ...data,
+      productId: Number(data.productId),
+    },
+  });
 
-    await prisma.product.update({
-        where : {id : Number(data.productId)},
-        data: {price_discount : priceDiscount}
-    })
+  await prisma.product.update({
+    where: { id: Number(data.productId) },
+    data: { price_discount: priceDiscount },
+  });
+}
+
+export async function list() {
+  return await prisma.discount.findMany();
+}
+
+export async function detailDiscount(id) {
+  return await prisma.discount.findUnique({
+    where: { id: Number(id) },
+  });
+}
+
+export async function edit(id, data) {
+  const discount = await detailDiscount(id);
+
+  if (!discount) {
+    throw new Error("discount tidak ditemukan");
   }
-  
-  export async function list() {
-    return await prisma.discount.findMany();
+
+  const product = await detailProduct(data.productId);
+
+  if (!product) throw new Error("Product tidak ditemukan");
+
+  const priceDiscount = product.price - (product.price * data.discount) / 100;
+
+  await prisma.discount.update({
+    where: { id: Number(id) },
+    data: {
+      ...data,
+      productId: data.productId ? Number(data.productId) : undefined,
+    },
+  });
+
+  await prisma.product.update({
+    where: { id: Number(data.productId) },
+    data: { price_discount: priceDiscount },
+  });
+}
+
+export async function deleted(id) {
+  const discount = await detailDiscount(id);
+
+  if (!discount) {
+    throw new Error("discount tidak ditemukan");
   }
-  
-  export async function detailDiscount(id) {
-    return await prisma.discount.findUnique({
-      where: { id: Number(id) },
-    });
-  }
-  
-  export async function edit(id, data) {
-    const discount = await detailDiscount(id);
-  
-    if (!discount) {
-      throw new Error("discount tidak ditemukan");
-    }
-  
-    return await prisma.discount.update({
-      where: { id: Number(id) },
-      data: { ...data },
-    });
-  }
-  
-  export async function deleted(id) {
-      const discount = await detailDiscount(id);
-  
-    if (!discount) {
-      throw new Error("discount tidak ditemukan");
-    }
-  
-    return await prisma.discount.delete({
-      where : {id : Number(id)}
-    })
-  }
+
+  return await prisma.discount.delete({
+    where: { id: Number(id) },
+  });
+}
