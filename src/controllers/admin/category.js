b7835e2f5980a66a7ee368b3d92ca4123356af4b
ariekas <ariariekas@gmail.com>
2025-11-30 22:47:57 +0700

    feat: implementasi swagger in category api

--- src/controllers/admin/category.js
+++ src/controllers/admin/category.js
@@ -1,5 +1,16 @@
 import { create, deleted, detail, edit, list } from "../../repositorys/category.js";
 
+/**
+ * @typedef {object} CreateCategory
+ * @property {string} name.required - name
+ */
+/**
+ * POST /admin/category
+ * @summary Create category
+ * @tags Admin Category
+ * @param {CreateCategory} request.body.required - category payload
+ * @return {object} 201 - success response
+ */
 export async function createCategory(req, res) {
   try {
     console.log(req.body)
@@ -18,6 +29,12 @@ export async function createCategory(req, res) {
   }
 }
 
+/**
+ * GET /admin/categorys
+ * @summary List category
+ * @tags Admin Category
+ * @return {object} 201 - success response
+ */
 export async function listCategory(req, res) {
     try {
         const category = await list(req.body)
@@ -43,6 +60,14 @@ export async function listCategory(req, res) {
     }
 }
 
+/**
+ * GET /admin/category/{id}
+ * @summary Get detail category
+ * @tags Admin Category
+ * @param {number} id.path.required - Category ID
+ * @return {object} 201 - success response
+ * @return {object} 404 - category not found
+ */
 export async function detailCategory(req, res) {
     try {
         const category = await detail(req.params.id)
@@ -68,6 +93,19 @@ export async function detailCategory(req, res) {
     }
 }
 
+/**
+ * @typedef {object} UpdateCategory
+ * @property {string} name.required - Category name
+ */
+/**
+ * PATCH /admin/category/{id}
+ * @summary Update category
+ * @tags Admin Category
+ * @param {number} id.path.required - Category ID
+ * @param {UpdateCategory} request.body.required - Category update payload
+ * @return {object} 201 - success response
+ * @return {object} 201.data - Updated category data
+ */
 export async function editCategory(req, res) {
     try {
         const category = await edit(req.params.id, req.body)
@@ -85,6 +123,13 @@ export async function editCategory(req, res) {
     }
 }
 
+/**
+ * DELETE /admin/category/{id}
+ * @summary Delete category
+ * @tags Admin Category
+ * @param {number} id.path.required - Category ID
+ * @return {object} 201 - success response
+ */
 export async function deleteCategory(req, res) {
     try {
         const category = await deleted(req.params.id)
